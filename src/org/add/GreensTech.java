8aad323e4f0c0cfbbce9e8056af20f95460ccc7b
subama1 <subashini.mohandas@ymail.com>
2021-10-19 17:23:33 +0530

    greenaddress project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public class GreensTech {
+	
+	public void greensOmr()   {
+	 System.out.println("Greens in OMR");
+	}
+	public void greensAdayar()  {
+	 System.out.println("Greens in Adayar");
+	}
+	public void greensTambaram()   {
+		System.out.println("Greens in Tambaram");
+	}
+	public void greensVelachery()   {
+	   System.out.println("Greens in Velachery");
+	}
+	public void greensAnnanagar()  {
+		System.out.println("Greens in Anna nagar");
+	}
+	public static void main(String[] args)  {
+		GreensTech gt = new GreensTech();
+		gt.greensOmr();
+		gt.greensAdayar();
+		gt.greensTambaram();
+		gt.greensVelachery();
+		gt.greensAnnanagar();
+	}
+}
